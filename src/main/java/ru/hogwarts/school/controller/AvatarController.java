09cffbe9de5e12dbdb53e15266fa00d877a1d703
Roman <Gushlenko63@mail.ru>
2026-08-07 16:16:29 +0400

    feat: implement downloadAvatarFromDb GET endpoint in AvatarController

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,10 +1,13 @@
 package ru.hogwarts.school.controller;
 
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.service.AvatarService;
 
 import java.awt.*;
@@ -30,5 +33,17 @@ public class AvatarController {
     )
             throws IOException {
         return avatarService.uploadAvatar(studentId, file);
+    }
+
+    @GetMapping("/{studentId}/from-db")
+    public ResponseEntity<byte[]> downloadAvatarFromDb(@PathVariable long studentId) {
+        Avatar avatar = avatarService.findAvatar(studentId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(headers)
+                .body(avatar.getData());
     }
 }
