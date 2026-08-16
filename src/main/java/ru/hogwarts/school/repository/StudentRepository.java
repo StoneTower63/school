de1f194a58add221f291b1c9be3678d6fa74f2eb
Roman <Gushlenko63@mail.ru>
2026-08-16 16:44:20 +0400

    feat: add native SQL queries for student aggregates

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Student;
 
@@ -9,4 +10,15 @@ import java.util.List;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByAgeBetween(int min, int max);
+
+    @Query(value = "SELECT COUNT(*) FROM student", nativeQuery = true)
+    Long getCountOfAllStudents();
+
+    @Query(value = "SELECT AVG(age) FROM student", nativeQuery = true)
+    Double getAverageAgeOfStudents();
+
+    @Query(value = "SELECT * FROM student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    List<Student> getLastFiveStudents();
+
+
 }
